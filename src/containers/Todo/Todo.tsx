72bd4322235e59ef6f33445b9f543043f44c3fb6
Chernyshov <xpom55@mail.ru>
2019-06-25 00:45:55 +0300

    + todo can delete task

--- src/containers/Todo/Todo.tsx
+++ src/containers/Todo/Todo.tsx
@@ -1,21 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, KeyboardEvent } from 'react';
 import { styled } from 'linaria/react';
 
 const Todo = () => {
   const [tasks, changeTasks] = useState(['Цветы полить', 'В магазин сходить', 'Почитать']);
   const [inputTask, changeInputTask] = useState('');
 
-  const onEnter = (event) => {
+  const onEnter = (event: React.ChangeEvent<HTMLInputElement> & KeyboardEvent) => {
     if (event.key === 'Enter') {
       const value = event.target.value;
-      console.log(event.target.value);
       changeTasks([...tasks, value]);
       changeInputTask('');
     }
   };
-  const onInputChange: React.KeyboardEventHandler = ({ target: { value } }) => {
-    console.log(value);
+  const onInputChange = ({ target: { value } }: React.ChangeEvent<HTMLInputElement>) =>
     changeInputTask(value);
+
+  const onDelete = (index: number) => () => {
+    console.log('index', index);
+    changeTasks(tasks.filter((_, innerIndex) => innerIndex !== index));
   };
 
   return (
@@ -23,7 +25,10 @@ const Todo = () => {
       <Tasks>
         <span>Todo:</span>
         {tasks.map((task: string, index: number) => (
-          <Task key={index}>{`${index + 1}. ${task}`}</Task>
+          <TaskRow>
+            <Minus className="heavy" onClick={onDelete(index)} />
+            <Task key={index}>{`${index + 1}. ${task}`}</Task>
+          </TaskRow>
         ))}
       </Tasks>
       <Input
@@ -44,9 +49,38 @@ const Wrapper = styled.div`
 const Tasks = styled.div`
   border: 1px solid gray;
 `;
+const TaskRow = styled.div`
+  display: flex;
+  padding-left: 0.5rem;
+`;
+const Minus = styled.div`
+  position: relative;
+  display: flex;
+  alight-items: center;
+  justify-content: center;
+  height: 1rem;
+  width: 1rem;
+  cursor: pointer;
 
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    height: 1px;
+    width: 80%;
+    top: 45%;
+    left: 5%;
+    background: white;
+  }
+  &:before {
+    transform: rotate(45deg);
+  }
+  &:after {
+    transform: rotate(-45deg);
+  }
+`;
 const Task = styled.div`
-  margin-left: 1rem;
+  margin-left: 0.5rem;
 `;
 
 const Input = styled.input`
